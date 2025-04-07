3c394f33ed99dad6b9fbe706eec6478154f15b52
Edgar Hernandez Garcia <edgar.hernandez@uanl.edu.mx>
2025-04-07 15:49:08 -0600

    prueba clase cuadrado ok

--- demo.py
+++ demo.py
@@ -7,3 +7,9 @@ class Cuadrado:
 
 Numlado = Cuadrado(50)
 print("El area del cuadrado es:", Numlado.area())
+
+##########################################################
+lado = int(input("Ingresa el lado?"))
+mi_ejemplo = Cuadrado(lado)
+r = mi_ejemplo.area()
+print(f"El area es {r}")
