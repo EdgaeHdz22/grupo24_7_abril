d416e86ebe6ec7c251b0e15f35a2dc1f15e1178f
Edgar Hernandez Garcia <edgar.hernandez@uanl.edu.mx>
2025-04-07 15:30:19 -0600

    se hizo la clase Cuadrado

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
 class Cuadrado:
-    pass 
+    def __init__(self,lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado * self.lado
+
+Numlado = Cuadrado(50)
+print("El area del cuadrado es:", Numlado.area())
